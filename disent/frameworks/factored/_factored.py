dff9c4cd518a84e3b6f1db1e47cd854b9e05c832
Cam Allen <csal@brown.edu>
2022-08-31 18:01:36 -0400

    Update paths for refactor & run yapf

--- disent/frameworks/factored/_factored.py
+++ disent/frameworks/factored/_factored.py
@@ -4,8 +4,8 @@ import os
 import platform
 import torch
 
-from markov_abstr.gridworld.models.nnutils import Network
-from factored_reps.models.cae import CAENet
+from factored_reps.models.nnutils import Network
+from factored_reps.models.factored.cae import CAENet
 from ..markov import MarkovAbstraction
 
 class FactoredModel(Network):
@@ -23,11 +23,11 @@ class FactoredModel(Network):
         # tag = 'exp21-cae-best__lr_0.001__Lrecz_1.0__distmode_mse__Lfoc_0.0__latentdims_10__Lrecxaug_0.0'  ##
         # tag = 'exp21-cae-best__lr_0.001__Lrecz_1.0__distmode_mse__Lfoc_0.0__latentdims_10__Lrecxaug_1.0'
 
-        prefix = '~/data-gdk/csal/factored/' if platform.system() == 'Linux' else '~/dev/factored-reps/'
+        prefix = '~/data-gdk/csal/factored/' if platform.system() == 'Linux' else '~/dev/factored-reps/' # yapf: disable
         expanded_prefix = os.path.expanduser(prefix)
         results_dir = expanded_prefix + 'results/'
 
-        args_filename = glob.glob(results_dir + 'focused-taxi/logs/{}/args-{}.txt'.format(tag, seed))[0]
+        args_filename = glob.glob(results_dir + f'focused-taxi/logs/{tag}/args-{seed}.txt')[0]
         with open(args_filename, 'r') as args_file:
             args = eval(args_file.read())
 
@@ -35,10 +35,10 @@ class FactoredModel(Network):
         print('device: {}'.format(self.device))
 
         self.facnet = CAENet(args,
-                n_actions=5,
-                n_input_dims=self.markov.latent_dims,
-                n_latent_dims=args.latent_dims,
-                device=self.device).to(self.device)
+                             n_actions=5,
+                             n_input_dims=self.markov.latent_dims,
+                             n_latent_dims=args.latent_dims,
+                             device=self.device).to(self.device)
         model_file = results_dir + 'focused-taxi/models/{}/focused-autoenc-{}_best.pytorch'.format(
             tag, args.seed)
         self.facnet.load(model_file, to=self.device)
--- disent/frameworks/markov/_markov_abstraction.py
+++ disent/frameworks/markov/_markov_abstraction.py
@@ -4,8 +4,8 @@ import os
 import platform
 import torch
 
-from markov_abstr.gridworld.models.nnutils import Network
-from markov_abstr.gridworld.models.featurenet import FeatureNet
+from factored_reps.models.nnutils import Network
+from factored_reps.models.markov.featurenet import FeatureNet
 
 class MarkovAbstraction(Network):
     def __init__(self, x_shape):
@@ -13,12 +13,11 @@ class MarkovAbstraction(Network):
         assert x_shape == (3, 84, 84)
 
         markov_abstraction_tag = 'exp78-blast-markov_122__learningrate_0.001__latentdims_20'
-        prefix = '~/data-gdk/csal/factored/' if platform.system() == 'Linux' else '~/dev/factored-reps/'
+        prefix = '~/data-gdk/csal/factored/' if platform.system() == 'Linux' else '~/dev/factored-reps/' # yapf: disable
         expanded_prefix = os.path.expanduser(prefix)
         results_dir = expanded_prefix + 'results/'
 
-        args_filename = glob.glob(results_dir +
-                                'taxi/logs/{}/args-*.txt'.format(markov_abstraction_tag))[0]
+        args_filename = glob.glob(results_dir + f'taxi/logs/{markov_abstraction_tag}/args-*.txt')[0] # yapf: disable
         with open(args_filename, 'r') as args_file:
             markov_args = eval(args_file.read())
 
@@ -27,10 +26,10 @@ class MarkovAbstraction(Network):
         print('device: {}'.format(self.device))
 
         self.featurenet = FeatureNet(markov_args,
-                                n_actions=5,
-                                input_shape=x_shape,
-                                latent_dims=markov_args.latent_dims,
-                                device=self.device).to(self.device)
+                                     n_actions=5,
+                                     input_shape=x_shape,
+                                     latent_dims=markov_args.latent_dims,
+                                     device=self.device).to(self.device)
         model_file = results_dir + 'taxi/models/{}/fnet-{}_best.pytorch'.format(
             markov_abstraction_tag, markov_args.seed)
         self.featurenet.load(model_file, to=self.device)
